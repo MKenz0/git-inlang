d7c74905a0947277e3bb7ea4a44327e382e0d67e
miguel <miguel7vj@gmail.com>
2023-10-11 10:38:22 +0200

    feat: :sparkles: Create SvelteKit project

---
+++ git-inlang/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import {i, languages, switchLanguage} from '@inlang/sdk-js';
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each languages as lang} 
+  <button on:click={() => switchLanguage(lang)}>{lang}</button>
+{/each}
+
+<h1>{i('message', {name: 'house'})}</h1>
